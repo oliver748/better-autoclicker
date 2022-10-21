078878a7707ecc88087eda7ff358a058675c3f3c
Oliver <113297002+oliver748@users.noreply.github.com>
2022-10-22 01:16:03 +0200

    exit key, sound when toggling on/off, +readability

--- main.py
+++ main.py
@@ -1,47 +1,60 @@
-import win32api, win32con, time
-from utils import key_state, switch_bool, clear
+import win32api, win32con
+from time import sleep
 from random import randint
+from utils import key_state, switch_bool, clear
+from playsound import playsound
+
 
 TOGGLE_KEY = "½"
+EXIT_KEY = "END"
+
 
 def console_log(state, click_delay):
+    clear()
     if state:
-        clear()
-        print(f"TOGGLED\nclick delay: {click_delay} ms")
+        print(f"toggled\nclick delay: {click_delay} ms")
     else:
-        clear()
-        print("NOT TOGGLED")
+        print("not toggled")
 
-def autoclicker(delay, deviation, before_delay):
-    def mouse_state():
-        if win32api.GetKeyState(0x01) == -128 or win32api.GetKeyState(0x01) == -127:
-            return True
+
+def mouse_state():
+    if win32api.GetKeyState(0x01) < -1:
+        return True
+
+
+def autoclicker(delay, deviation, before_delay, sound):
     toggle = True
     console_log(toggle, "?")
-    while 1:
+    while not key_state(EXIT_KEY):
         if toggle and mouse_state():
-            time.sleep(before_delay/1000)
-            while 1:
+            sleep(before_delay / 1000)
+            while not key_state(EXIT_KEY):
                 if mouse_state():
-                    click_delay = randint(delay-deviation, delay+deviation)
+                    click_delay = randint(delay - deviation, delay + deviation)
                     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
                     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
-                    time.sleep(click_delay/1000)
-                    console_log(1, click_delay)
+                    sleep(click_delay / 1000)
+                    console_log(True, click_delay)
                 else:
                     break
         if key_state(TOGGLE_KEY):
             toggle = switch_bool(toggle)
             console_log(toggle, "?")
+            if play_sound == "y":
+                if toggle: playsound('sounds/toggled_on.mp3')
+                else: playsound('sounds/toggled_off.mp3')
             while key_state(TOGGLE_KEY):
                 pass
 
-        time.sleep(0.01)
+        sleep(0.01)
+
 
 if __name__ == "__main__":
     clear()
-    print("recommended settings: clicking delay 30-35ms, deviation 5-7ms, before_delay 125ms\n")
-    delay = int(input('clicking delay (ms): '))
-    deviation = int(input('deviation (ms): '))
-    before_delay = int(input('delay before clicking (ms): '))
-    autoclicker(delay, deviation, before_delay)
+    print("recommended settings:",
+            "click delay 30-35ms, deviation 5-7ms, before delay 125ms\n",)
+    delay = int(input("click delay (ms): "))
+    deviation = int(input("deviation (ms): "))
+    before_delay = int(input("delay before clicking (ms): "))
+    play_sound = str(input('play sound when toggling (y/n): ')).lower()
+    autoclicker(delay, deviation, before_delay, play_sound)
